5cea8298da1ffdfc1e8369a066f02b2e9644faaf
Neelanshi Sharma <sharmaneelanshi00@gmail.com>
2020-10-23 22:32:09 +0530

    Doctor HomePage

--- app/src/main/java/com/example/blackcoffer_neelanshi/HomeActivity.java
+++ app/src/main/java/com/example/blackcoffer_neelanshi/HomeActivity.java
@@ -99,6 +99,11 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_profile) {
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+            return true;
+        }
+
         if (id == R.id.action_settings) {
             return true;
         }
